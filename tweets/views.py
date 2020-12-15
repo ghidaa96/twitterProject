e1c440db0fbda209e1c3afa76452f30e1da05dfe
ghidaa96 <ghidnass96@gmail.com>
2020-12-15 11:14:53 +0300

    add models

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Tweet
 # Create your views here.
+def home_view(request , *arg,**kwargs):
+    return HttpResponse("<h1>Hello World</h1>")
+def tweet_detile(request , tweet_id,*arg,**kwargs):
+    obj = Tweet.objects.get(id=tweet_id)
+    return HttpResponse(f"<h1>Hello {tweet_id} {obj.content}</h1>")
